75d4a01722a71a824e62c6de86508d99c0415305
El Bixos Delapaegas <vpage.vincent@gmail.com>
2018-10-13 18:12:25 -0400

    Still Playing with positions

--- sources/02_mygamefast/05_camera.py
+++ sources/02_mygamefast/05_camera.py
@@ -9,15 +9,15 @@ class MyApp(ShowBase):
         self.world.reparentTo(self.render)
 
         self.player = self.loader.loadModel("alliedflanker.egg")
-        self.player.setPos(220,220,85)
-        self.player.setH(90)
+        self.player.setPos(0,220,85)
+        #self.player.setH(90)
         self.player.reparentTo(self.render)
 
 
         # performance (to be masked later by fog) and view:
-        self.maxdistance = 400
-        self.camLens.setFar(self.maxdistance)
-        self.camLens.setFov(60)
+        #self.maxdistance = 400
+        #self.camLens.setFar(self.maxdistance)
+        #self.camLens.setFov(60)
 
         base.disableMouse() # or updateCamera will fail!
         self.updateCamera()
@@ -26,6 +26,8 @@ class MyApp(ShowBase):
         # see issue content for how we calculated these:
         self.camera.setPos(self.player, 25.6225, 3.8807, 10.2779)
         self.camera.setHpr(self.player,94.8996,-16.6549,1.55508)
+        self.camera.setPos( 0, 0, 600)
+        self.camera.lookAt(Point3(320,320,0))
 
 app = MyApp()
 app.run()
