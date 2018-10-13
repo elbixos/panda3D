e170814b5ab0e51f15c51d68479868e485b385b2
El Bixos Delapaegas <vpage.vincent@gmail.com>
2018-10-13 16:14:17 -0400

    camera et camera + objet

--- sources/02_mygamefast/05_camera2.py
+++ sources/02_mygamefast/05_camera2.py
@@ -1,4 +1,5 @@
 from direct.showbase.ShowBase import ShowBase
+from math import pi, sin, cos
 
 
 class MyApp(ShowBase):
@@ -12,6 +13,13 @@ class MyApp(ShowBase):
         self.player.setPos(220,220,85)
         self.player.setH(90)
         self.player.reparentTo(self.render)
+
+
+        self.stuff = self.loader.loadModel("camera.egg")
+        self.stuff.setPos(self.player,0,0,10)
+        self.stuff.setHpr(90,-90,0)
+        self.stuff.reparentTo(self.render)
+
 
 
         # performance (to be masked later by fog) and view:
